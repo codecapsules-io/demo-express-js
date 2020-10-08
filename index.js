851d9dd3a5bc36dd7da6dceb615d44a580572521
dimcon <daimonsewell@gmail.com>
2020-10-08 13:44:34 +0200

    added site

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path")
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.static('static'))
 app.use(express.json());
 
 app.get("/api/data", (req, res) => {
@@ -14,5 +16,14 @@ app.get("/api/data", (req, res) => {
     ],
   });
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get("/index", (req, res) => {
+  res.redirect("/")
+});
+
 
 app.listen(port, () => console.log(`Listening on ${port}`));
